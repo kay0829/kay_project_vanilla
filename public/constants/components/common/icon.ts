3a0f01531319b2dc33600bfa04522cb1b267b934
kay0829 <eluka97@daum.net>
2023-04-21 17:57:14 +0900

    💄 3_모달_이벤트_추가) 아이콘 더블 클릭으로 모달창이 떴을 때 하단 상태표시바에 뜬 창 표시할 수 있도록 css 변경

--- public/constants/components/common/icon.ts
+++ public/constants/components/common/icon.ts
@@ -1,21 +1,27 @@
 export const ICON_INFO = {
-    INTERNET: {
-        type: "INTERNET",
+    // INTERNET: {
+    //     type: "INTERNET",
+    //     extenstion: "",
+    //     imgSrc: "./assets/icons/icon-internet.png",
+    //     explanation: "인터넷 아이콘",
+    // },
+    // FOLDER: {
+    //     type: "FOLDER",
+    //     extenstion: "",
+    //     imgSrc: "./assets/icons/icon-folder.png",
+    //     explanation: "폴더 아이콘",
+    // },
+    // TXT: {
+    //     type: "TXT",
+    //     extenstion: "txt",
+    //     imgSrc: "./assets/icons/icon-file-document.png",
+    //     explanation: "텍스트 파일 아이콘",
+    // },
+    CALCULATOR: {
+        type: "CALCULATOR",
         extenstion: "",
-        imgSrc: "./assets/icons/icon-internet.png",
-        explanation: "인터넷 아이콘",
-    },
-    FOLDER: {
-        type: "FOLDER",
-        extenstion: "",
-        imgSrc: "./assets/icons/icon-folder.png",
-        explanation: "폴더 아이콘",
-    },
-    TXT: {
-        type: "TXT",
-        extenstion: "txt",
         imgSrc: "./assets/icons/icon-file-document.png",
-        explanation: "텍스트 파일 아이콘",
+        explanation: "계산기 아이콘",
     },
 }
 
@@ -23,31 +29,40 @@ export const ICON_INFO = {
 * 기본 아이콘 정보
 */
 export const DEFAULT_ICON = [
+    // {
+    //     idx: 0,
+    //     name: "인터넷 익스플로러",
+    //     extenstion: ICON_INFO.INTERNET.extenstion,
+    //     type: ICON_INFO.INTERNET.type,
+    //     imgSrc: ICON_INFO.INTERNET.imgSrc,
+    //     explanation: ICON_INFO.INTERNET.explanation,
+    //     style: { gridRowStart: 1, gridColumnStart: 1 },
+    // },
+    // { 
+    //     idx: 1,
+    //     name: "내 문서",
+    //     extension: ICON_INFO.FOLDER.extenstion,
+    //     type: ICON_INFO.FOLDER.type,
+    //     imgSrc: ICON_INFO.FOLDER.imgSrc,
+    //     explanation: ICON_INFO.FOLDER.explanation,
+    //     style: { gridRowStart: 2, gridColumnStart: 1 },
+    // },
+    // {
+    //     idx: 2,
+    //     name: "파일.txt",
+    //     extension: ICON_INFO.TXT.extenstion,
+    //     type: ICON_INFO.TXT.type,
+    //     imgSrc: ICON_INFO.TXT.imgSrc,
+    //     explanation: ICON_INFO.TXT.explanation,
+    //     style: { gridRowStart: 3, gridColumnStart: 1 },
+    // },
     {
         idx: 0,
-        name: "인터넷 익스플로러",
-        extenstion: ICON_INFO.INTERNET.extenstion,
-        type: ICON_INFO.INTERNET.type,
-        imgSrc: ICON_INFO.INTERNET.imgSrc,
-        explanation: ICON_INFO.INTERNET.explanation,
-        style: { gridRowStart: 1, gridColumnStart: 1 },
-    },
-    { 
-        idx: 1,
-        name: "내 문서",
-        extension: ICON_INFO.FOLDER.extenstion,
-        type: ICON_INFO.FOLDER.type,
-        imgSrc: ICON_INFO.FOLDER.imgSrc,
-        explanation: ICON_INFO.FOLDER.explanation,
-        style: { gridRowStart: 2, gridColumnStart: 1 },
-    },
-    {
-        idx: 2,
-        name: "파일.txt",
-        extension: ICON_INFO.TXT.extenstion,
-        type: ICON_INFO.TXT.type,
-        imgSrc: ICON_INFO.TXT.imgSrc,
-        explanation: ICON_INFO.TXT.explanation,
-        style: { gridRowStart: 3, gridColumnStart: 1 },
+        name: "계산기",
+        extension: ICON_INFO.CALCULATOR.extenstion,
+        type: ICON_INFO.CALCULATOR.type,
+        imgSrc: ICON_INFO.CALCULATOR.imgSrc,
+        explanation: ICON_INFO.CALCULATOR.explanation,
+        style: { gridRowStart: 1, gridColumnStart: 1, backgroundPosition: "-48px -48px" },
     },
 ]
