37801eafee246d61bbc53a5aea37699fe84da5bf
kay0829 <eluka97@daum.net>
2023-05-16 15:18:44 +0900

    🔧 3_모달_이벤트_추가) 모달 관련 redux 코드 추가

--- public/src/reducer/modals/action.ts
+++ public/src/reducer/modals/action.ts
@@ -1,5 +1,12 @@
 import { createAction } from '@reduxjs/toolkit';
-import { ADD_MODAL } from "./actionType";
+
 import { MODAL } from "../../../types/reducer/index";
 
+import { ADD_MODAL, TOGGLE_FOCUS_MODAL, REMOVE_MODAL } from "./actionType";
+
+
 export const addModal = createAction<MODAL>(ADD_MODAL);
+
+export const toggleFocusModal = createAction<{modalIdx: number, focused: boolean}>(TOGGLE_FOCUS_MODAL);
+
+export const removeModal = createAction<{modalIdx: number}>(REMOVE_MODAL);
--- public/src/reducer/modals/actionType.ts
+++ public/src/reducer/modals/actionType.ts
@@ -1,1 +1,5 @@
 export const ADD_MODAL = 'MODALS/ADD_MODAL' as const;
+
+export const TOGGLE_FOCUS_MODAL = 'MODALS/TOGGLE_FOCUS_MODAL' as const;
+
+export const REMOVE_MODAL = 'MODALS/REMOVE_MODAL' as const;
